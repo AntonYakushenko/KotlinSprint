83aafd0bb96c20e2ec5f2b2da41b736db01ab41d
Toni773 <75640559+AntonYakushenko@users.noreply.github.com>
2023-07-19 00:16:49 +0400

    KS-1-3 declared a new variables and print them

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Год полета: $year")
+    println("Час полета: $hour")
+    println("Минута полета: $minute")
+
+    hour = 10
+    minute = 55
+
+    print("Посадка: $hour:$minute")
+}
