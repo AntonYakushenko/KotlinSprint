f15ca0c9bdfc752c3c4137aae0fa8805a4fb8d7f
Toni773 <75640559+AntonYakushenko@users.noreply.github.com>
2023-07-18 23:50:48 +0400

    KS-1-2 output to the console the values of all previously declared variables declared a new variable employeeCount comment variable employeeCount declared a new variable newEmployeeCount

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    val totalOrders: Int = 75
+    val purchaseThankYouText: String = "Спасибо за покупку! Мы рады, что вы выбрали наш интернет-магазин."
+
+    println("totalOrders: $totalOrders")
+    println("purchaseThankYouText: $purchaseThankYouText")
+
+    //val employeeCount: Int = 2000
+    //println("employeeCount: $employeeCount")
+
+    val newEmployeeCount: Int = 1999
+    println("newEmployeeCount: $newEmployeeCount")
+
+}
