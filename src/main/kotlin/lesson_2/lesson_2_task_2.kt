76c3ab979110818c7c2198f83e4caf46110ad300
Toni773 <75640559+AntonYakushenko@users.noreply.github.com>
2023-07-23 00:41:26 +0400

    KS-2-1 Calculate average score

---
+++ src/main/kotlin/lesson_2/lesson_2_task_2.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+
+    val score1 = 3
+    val score2 = 4
+    val score3 = 3
+    val score4 = 5
+
+    val average = (score1 + score2 + score3 + score4) / 4.0
+
+    println(String.format("%.2f", average))
+
+}
