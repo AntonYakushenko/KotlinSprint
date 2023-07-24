e093bd1359fb5034fdb99ac632e127ae6d8337dc
Toni773 <75640559+AntonYakushenko@users.noreply.github.com>
2023-07-25 00:25:59 +0400

    KS-2-5 Calculate compound interest

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,19 +1,19 @@
 package lesson_2
 
+import kotlin.math.pow
+
 
 fun main() {
     val summa = 70000.0
     val percent = 16.7 / 100
     val years = 20
 
-    var result = summa
-    for (i in 1..years) {
-        result *= (1 + percent)
-    }
-
+    val result = summa * (1 + percent).pow(years)
     val roundedResult = String.format("%.3f", result)
+
     println(roundedResult)
 }
+
 
 
 
